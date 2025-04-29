bb43ca75632af4b30b6ac103b30677db583502dc
c0c24040d1 <c0c24040d1@edu.teu.ac.jp>
2025-04-29 14:59:26 +0900

    prac4

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -163,8 +163,8 @@ def main():
                 # ゲームオーバー時に，こうかとん画像を切り替え，1秒間表示させる
                 bird.change_img(8, screen)
                 fonto = pg.font.Font(None,80)
-                txt = fonto.render("Game over", True, (255,0,0))
-                screen.blit(txt,[WIDTH//2-150, HEIGHT//2])
+                txt=fonto.render("Game Over",True,(255,0,0))
+                screen.blit(txt,[WIDTH//2-150,HEIGHT//2])
                 pg.display.update()
                 time.sleep(1)
                 return
